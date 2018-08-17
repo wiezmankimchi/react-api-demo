984f35657e13f8c91ddb6f649b3419749967f137
wiezmankimchi <wiezmankimchi@gmail.com>
2018-08-17 11:53:49 -0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-api-demo
-Created with CodeSandbox
---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>My Users</h2>
+        <p>The list comes here...</p>
+      </div>
+    );
+  }
+}
+
+export default App;
